7c1ead47e6a0610b66c7176e2374d87493855df0
Mtherezajdm <mtherezajdm@gmail.com>
2026-09-01 14:48:22 -0400

    Add files via upload

---
+++ 1080b.c
@@ -0,0 +1,39 @@
+/*Maria Thereza Jardim
+Este exercício lê n números, apresenta o maior dentre esses números e a posição desse usando alocação dinâmica de vetores*/
+
+#include <stdio.h>
+#include <stdlib.h>
+ 
+int main() {
+ 
+    int *v, i, maior, pos, n;
+
+    printf("Digite o tamanho do vetor: ");
+    scanf("%d", &n); // descobrimos o tamanho do vetor
+    v = (int *) malloc(n * sizeof(int)); // ponteiro vai apontar para n inteiros
+
+    if(v == NULL) { // verifica se o malloc não conseguiu alocar a memória
+        printf("Erro de alocação!\n");
+        return 1;
+    }
+             
+    printf("Digite os números inteiros a serem analisados: ");
+    for(i = 0; i < n; i++) {
+        scanf("%d\n", &v[i]); // leitura do vetor
+    }
+        
+    maior = v[0];
+    pos = 0;
+
+    for(i = 1; i < n; i++) { // a partir do segundo termo, compara o maior com o atual
+        if(v[i] > maior) { // se houver algum maior que o "maior", o valor é substituido
+            maior = v[i];
+            pos = i;
+        }
+    }
+    printf("%d\n%d\n", maior, pos + 1); // imprime o maior número e sua posição
+    
+    free(v);
+
+    return 0;
+}
